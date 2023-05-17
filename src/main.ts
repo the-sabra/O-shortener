7a3237403fee8d072c377849749505248dcd3f41
omarsabra1 <omarsabra509@gmail.com>
2023-05-17 13:51:06 +0300

    adding port env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
